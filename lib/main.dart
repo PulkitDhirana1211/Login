2867a45c7c65be6530f6ec3a465c2d41b53ed606
PulkitDhirana1211 <54793200+PulkitDhirana1211@users.noreply.github.com>
2021-11-09 19:48:58 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
-import 'package:login/SecondPage.dart';
 import 'package:login/SecondPage.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
